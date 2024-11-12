20d99ee8ecfcef259be995a85397e0768998e96e
JavelinSX <zuev499@gmail.com>
2024-11-12 20:58:18 +0300

    test

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
+// next.config.ts
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export', // Включаем статическую генерацию
+  basePath: '/three', // Замените на имя вашего репозитория
+  images: {
+    unoptimized: true, // Необходимо для статической генерации
+  },
   reactStrictMode: false,
   transpilePackages: ['three'],
   webpack: (config) => {
